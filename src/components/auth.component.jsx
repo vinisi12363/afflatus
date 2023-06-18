8c2370868ddb9675f3ee634728583785a5584a5d
vinisi12363 <viniciuspv.si@gmail.com>
2023-06-18 19:48:52 -0300

    adding the login page

--- src/components/auth.component.jsx
+++ src/components/auth.component.jsx
@@ -1,13 +1,97 @@
+import { Form } from 'react-router-dom'
+import styled from 'styled-components'
+import { Navigate } from 'react-router-dom'
 
+import foto1 from '../assets/momAfflatus.jpg'
 
-export default function LoginPage(){
+export default function LoginBody(){
 
     return(
-        <LoginContainer>
-
-        </LoginContainer>
+        <PageContainer>
+                <LoginContainer>
+                <h1> Afflatus </h1>
+                        <LoginPanel>
+                                <form>
+                                    <StyledInput1 placeholder='usuario' type='text' required ></StyledInput1>
+                                    <StyledInput1 placeholder='senha' type= 'password' required></StyledInput1>
+                                    <button type='submit'onClick={()=>{alert("clicado")}}> Log In</button>
+                                </form>
+                        </LoginPanel>
+                        <h1> Assistant </h1>
+                </LoginContainer>
+             
+        </PageContainer>
 
     )
 
-
 }
+
+
+const LoginPanel = styled.div`
+    display:flex;
+    flex-direction:column;
+
+    form{
+        align-items:center;
+        background-color:sandybrown;
+        border:3px black solid;
+        border-radius:5px;
+        display:flex;
+        flex-direction: column;
+        height:60dvh;
+        width:400px;
+
+        button{
+            margin-top:20px;
+            width:150px;
+            height:50px;
+            font-size:20px;
+            border:3px solid;
+            color:white;
+            background:#333333;
+
+           
+        }
+    }
+   
+
+`
+const StyledInput1= styled.input`
+    
+    margin-top:30px;
+    width:300px;
+    height:60px;
+    border: 3px solid black;
+    border-radius:5px;
+    text-align: center;
+    font-size:25px;
+    background-color:#dbdbdb;
+
+`
+
+const PageContainer  = styled.div`
+    width:100%;
+    min-height:100dvh;
+    background-color: black;
+    align-items: center;
+    
+`
+
+const LoginContainer = styled.div `
+
+
+  
+    display:flex;
+    flex-direction:column;
+    
+ 
+    h1{
+        font-size:60px;
+        color:black;
+    }
+    background-image: url("https://www.verywellfamily.com/thmb/XNQPqjgDTmu9RB9zdm6tebjF3hM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-482136905-2-574320f63df78c6bb016da13.jpg"); 
+    background-repeat:no-repeat;
+    background-size:cover;
+   
+`
+
---
+++ src/pages/Login.page.jsx
@@ -0,0 +1,10 @@
+import LoginBody from "../components/auth.component.jsx";
+
+export default function LoginPage(){
+
+    return (
+            <LoginBody/>
+
+    )
+
+}
