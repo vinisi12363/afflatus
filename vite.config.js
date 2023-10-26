0e7727ce43fd58d16cd1a8025950d6fa44e6e21e
vinisi12363 <viniciuspv.si@gmail.com>
2023-10-26 10:38:59 -0300

    feat: adding the dist folder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base:'/vite-gh-pages/',
   plugins: [react()],
   resolve: {
     alias: {
